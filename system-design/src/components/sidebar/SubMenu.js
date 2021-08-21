14a8d1e70e533d2ca24f376a4927d229afa42120
Kapil Gupta <kapilguptansit@gmail.com>
2021-08-21 10:29:51 +0000

    Corrected Sidebar issue

--- system-design/src/components/sidebar/SubMenu.js
+++ system-design/src/components/sidebar/SubMenu.js
@@ -55,6 +55,9 @@ export const SubMenu = ({ item }) => {
                 <div>
                     {item.subNav && (subnav ? item.iconOpened: item.iconClosed)}
                 </div>
+
+
+            </SidebarLink>
                 {subnav && item.subNav.map((item, index) => {
                     return (
                         <DropdownLink to={item.path} key={index}>
@@ -63,8 +66,6 @@ export const SubMenu = ({ item }) => {
                         </DropdownLink>
                     )
                 })}
-
-            </SidebarLink>
             
         </div>
     )
