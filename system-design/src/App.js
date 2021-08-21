d623bc79eddf5f868c054d9a5e9ed985d29b70d2
Kapil Gupta <kapilguptansit@gmail.com>
2021-08-21 10:22:46 +0000

    Added Sidebar

--- system-design/src/App.js
+++ system-design/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Overview } from './pages/Overview';
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar/>
-    </div>
+      <Sidebar/>
+      <Route path='/overview' exact component={Overview} />
+      <Route path='/reports' exact component={Reports} />
+      <Route path='/reports/reports1' exact component={ReportsOne} />
+      <Route path='/reports/reports2' exact component={ReportsTwo} />
+      <Route path='/reports/reports3' exact component={ReportsThree} />
+    </Router>
   );
 }
 
